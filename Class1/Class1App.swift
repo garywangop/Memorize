4cabd2164863c63a98738b7c910ea9e3788c424b
garywangop <garywangop@gmail.com>
2020-10-14 20:14:34 -0400

    Class 1

---
+++ Class1/Class1App.swift
@@ -0,0 +1,17 @@
+//
+//  Class1App.swift
+//  Class1
+//
+//  Created by Ge Wang on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct Class1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Class1/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Class1
+//
+//  Created by Ge Wang on 10/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        return HStack {
+            ForEach(0..<4) {index in
+                cardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+        
+    }
+}
+
+struct cardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("🦧")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
