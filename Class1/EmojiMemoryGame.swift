3d0f8741c1c28a4debeddb58060c42b331af4295
garywangop <garywangop@gmail.com>
2020-10-17 21:01:47 -0400

    Make emojis larger

--- Class1/EmojiMemoryGame.swift
+++ Class1/EmojiMemoryGame.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 // This is the View
 
-class EmojiMemoryGame {
+// ObservableObject: 放了这个关键词，model里faceup变了之后就能在view model里相应的变化了
+class EmojiMemoryGame: ObservableObject {
     // 创造个access model的变量，直接取名叫model
     // 最好不要用model这个名字，因为这是个concept，叫game这种描述性的名字比较好，不然别人看起来很困惑
     // emoji的type是String，所以这里用String
@@ -21,7 +22,8 @@ class EmojiMemoryGame {
      })
      */
     // 根据swift的特性，简化后就成为了下面这个样子
-    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    // @Published：var model只要change了就broadcast the whole world
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
     static func createMemoryGame() -> MemoryGame<String> {
         let emojis: Array<String> = ["🐶", "🐰", "🐻"]
@@ -37,6 +39,9 @@ class EmojiMemoryGame {
     
     // MARK: - Intent(s)
     func choose(card: MemoryGame<String>.Card) {
+        // object change之后，用下面这个send（）的method告诉全世界并publish
+        // 但是object会老change，每个change的地方写下面这个东西就太麻烦了，所以写在var model那，只要model change了就broadcast
+        // objectWillChange.send()
         model.choose(card: card)
     }
 }
---
+++ Class1/EmojiMemoryGameView.swift
@@ -0,0 +1,56 @@
+//
+//  EmojiMemoryGameView.swift
+//  Class1
+//
+//  Created by Ge Wang on 10/13/20.
+//
+
+import SwiftUI
+
+// This is ViewModel
+struct EmojiMemoryGameView: View {
+    // 只要检测到了object change，那么@ObservedObject就redraw一下
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        return HStack {
+            ForEach(viewModel.cards) {card in
+                CardView(card: card).onTapGesture (perform: {viewModel.choose(card: card)})
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        
+    }
+}
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        GeometryReader(content : { geometry in
+            ZStack {
+                if card.isFaceUp {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                    Text(card.content)
+                } else {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill()
+                }
+            }
+            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * fontScaleFactor))
+        })
+        
+    }
+    
+    // MARK: - Drawing Constants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    let fontScaleFactor: CGFloat = 0.75
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+    }
+}
